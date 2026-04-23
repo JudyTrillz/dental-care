78bb916e591239a505d3580d416892dba991ee8b
Judy Odiakose <ojudy007@gmail.com>
2026-04-23 14:51:14 +0100

    Updated my server.js and git ignore file.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes/index");
 const dentistRoutes = require("./routes/dentistRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const seedServices = require("./utils/seedServices");
+const authRoutes = require("./routes/auth");
+const publicRoutes = require("./routes/public");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,20 +18,30 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "https://dentalcare-app.netlify.app", // frontend
+      // "https://dentalcare-app.netlify.app", // live frontend
+      "http://127.0.0.1:5500", // your local frontend if you open index.html via Live Server
+      "http://localhost:5500", // alternate Live Server localhost
+      "http://localhost:5000", // backend port itself if needed
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api", routes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/dentists", dentistRoutes);
 app.use("/api/bookings", bookingRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//PUBLIC API FOR FRONTEND
+app.use("/api/public", publicRoutes);
 
 // Seed services and start server
 const startServer = async () => {
