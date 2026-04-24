efd39cbe066cd11bbb9f8e8e5fb599687c7a33dd
Judy Odiakose <ojudy007@gmail.com>
2026-04-24 09:08:42 +0100

    refactored to only frontend leaving backend in separate folder

--- controllers/healthControllers.js
+++
@@ -1,9 +0,0 @@
-const getHealth = (req, res) => {
-  try {
-    res.json({ status: "server running" });
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
-  }
-};
-
-module.exports = { getHealth };
